cf9b40c5f060e6e70953e8bfa59063dd9bd2132d
Nick McDonnough <nickmcd@gmail.com>
2014-10-14 12:00:55 -0500

    Add test for missing photo in mms

--- spec/use_cases/process_photo_spec.rb
+++ spec/use_cases/process_photo_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper.rb'
 
 describe Airpic::UseCases::ProcessPhoto do
+  it 'will return an error option if no photo is attached' do
+    params = {:theres_no_image => :here}
+    result = Airpic::UseCases::ProcessPhoto.run(params)
+    expect(result.success?).to be false
+  end
+
   it 'will correctly resize the photo and return it\'s location' do
     image_location = 'spec/fixtures/atlas_maple.png'
     params = {'images' => [{'image' => image_location}]}
